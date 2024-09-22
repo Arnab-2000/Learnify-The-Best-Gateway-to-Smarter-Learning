ece7cef0962059a2a4af811c61ff1c760f90a4e4
Arnab-2000 <arnabmukherjee486@gmail.com>
2024-09-23 02:02:59 +0530

    written Custom Sql queries

--- src/main/java/com/learnify/repository/ContactRepository.java
+++ src/main/java/com/learnify/repository/ContactRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,12 +16,18 @@ import java.util.List;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
-    List<Contact> findByStatus(String status);
-
+//    List<Contact> findByStatus(String status);
+//
     Page<Contact> findByStatus(String status, Pageable pageable);
 
     @Transactional
     @Modifying
     @Query("UPDATE Contact c SET c.status = :status WHERE c.contactId = :contactId")
     int updateStatusById(String status, int contactId);
+
+//    Page<Contact> findOpenMsgs(@Param("status") String status, Pageable pageable);
+//
+//    @Transactional
+//    @Modifying
+//    int updateMsgStatus(String status, int contactId);
 }
